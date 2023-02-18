486d4cae08ba46b895db17d116ecf24ff87c4079
Joseph Woodall <56406362+RedLeadr@users.noreply.github.com>
2023-02-18 10:35:26 -0600

    adds new button to navigation menu

--- client/src/components/NavigationMenu.js
+++ client/src/components/NavigationMenu.js
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 export default function NavigationMenu() {
   return (
@@ -10,8 +9,22 @@ export default function NavigationMenu() {
         <Container>
           <Navbar.Brand href="home">Joseph Woodall</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/github_repositories">Github Repositories</Nav.Link>
+            <Nav.Link href="/home">
+              <button data-text="Awesome" class="button">
+                <span class="actual-text">&nbsp;Home&nbsp;</span>
+                <span class="hover-text" aria-hidden="true">
+                  &nbsp;Home&nbsp;
+                </span>
+              </button>
+            </Nav.Link>
+            <Nav.Link href="/github_repositories">
+              <button data-text="Awesome" class="button">
+                <span class="actual-text">&nbsp;Github Repositories&nbsp;</span>
+                <span class="hover-text" aria-hidden="true">
+                  &nbsp;Github Repositories&nbsp;
+                </span>
+              </button>
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
