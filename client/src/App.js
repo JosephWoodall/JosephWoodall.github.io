bcf7b97748f6653d456cb7f9469693ebfd02f928
Joseph Woodall <josephlw4@gmail.com>
2023-02-22 13:20:33 -0600

    fixes nav.link

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,4 @@
 import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 
 import logo from "./logo.svg";
 import "./App.css";
@@ -19,40 +16,18 @@ export default function App() {
   return (
     <>
       <HashRouter basename="/">
-
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="home">Joseph Woodall</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="/home">
-                <button data-text="Awesome" class="button">
-                  <span class="actual-text">&nbsp;Home&nbsp;</span>
-                  <span class="hover-text" aria-hidden="true">
-                    &nbsp;Home&nbsp;
-                  </span>
-                </button>
-              </Nav.Link>
-              <Nav.Link href="/github_repositories">
-                <button data-text="Awesome" class="button">
-                  <span class="actual-text">&nbsp;Github_Highlights&nbsp;</span>
-                  <span class="hover-text" aria-hidden="true">
-                    &nbsp;Github_Highlights&nbsp;
-                  </span>
-                </button>
-              </Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
+        <NavigationMenu />
         <div className='body-background'>
           <Routes>
             <Route index element={<Home />} />
-            <Route exact path="home" element={<Home />} />
-            <Route exact path="github_repositories" element={<GithubRepositories />} />
-            <Route exact path="*" element={<NoPage />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route path="/github_repositories" element={<GithubRepositories />} />
+            <Route path="/*" element={<NoPage />} />
           </Routes>
         </div>
         <Footer />
       </HashRouter>
+
     </>
   );
 }
--- client/src/components/NavigationMenu.js
+++ client/src/components/NavigationMenu.js
@@ -1,15 +1,16 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 export default function NavigationMenu() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="home">Joseph Woodall</Navbar.Brand>
+          <Navbar.Brand as={Link} to="home">Joseph Woodall</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/home">
+            <Nav.Link as={Link} to="/home">
               <button data-text="Awesome" class="button">
                 <span class="actual-text">&nbsp;Home&nbsp;</span>
                 <span class="hover-text" aria-hidden="true">
@@ -17,7 +18,7 @@ export default function NavigationMenu() {
                 </span>
               </button>
             </Nav.Link>
-            <Nav.Link href="/github_repositories">
+            <Nav.Link as={Link} to="/github_repositories">
               <button data-text="Awesome" class="button">
                 <span class="actual-text">&nbsp;Github_Highlights&nbsp;</span>
                 <span class="hover-text" aria-hidden="true">
