b4af7328d748411bf8b46da0d61997d822ee3242
Joseph Woodall <josephlw4@gmail.com>
2023-02-22 16:08:47 -0600

    changes css layout, first threejs viz

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,8 +8,8 @@ import GithubRepositories from "./pages/GithubRepositories";
 import NoPage from "./pages/NoPage";
 
 import NavigationMenu from "./components/NavigationMenu";
-import Quote from "./components/Quote";
 import Footer from "./components/Footer";
+
 
 export default function App() {
   return (
