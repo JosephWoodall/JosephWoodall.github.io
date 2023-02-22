8935806d056d4fdd73097427d3779b055c3ca931
Joseph Woodall <josephlw4@gmail.com>
2023-02-22 12:46:25 -0600

    fixes routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 import logo from "./logo.svg";
 import "./App.css";
@@ -16,13 +19,36 @@ export default function App() {
   return (
     <>
       <HashRouter basename="/">
-        <NavigationMenu />
+
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="home">Joseph Woodall</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/home">
+                <button data-text="Awesome" class="button">
+                  <span class="actual-text">&nbsp;Home&nbsp;</span>
+                  <span class="hover-text" aria-hidden="true">
+                    &nbsp;Home&nbsp;
+                  </span>
+                </button>
+              </Nav.Link>
+              <Nav.Link href="/github_repositories">
+                <button data-text="Awesome" class="button">
+                  <span class="actual-text">&nbsp;Github_Highlights&nbsp;</span>
+                  <span class="hover-text" aria-hidden="true">
+                    &nbsp;Github_Highlights&nbsp;
+                  </span>
+                </button>
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
         <div className='body-background'>
           <Routes>
             <Route index element={<Home />} />
-            <Route path="home" element={<Home />} />
-            <Route path="github_repositories" element={<GithubRepositories />} />
-            <Route path="*" element={<NoPage />} />
+            <Route exact path="home" element={<Home />} />
+            <Route exact path="github_repositories" element={<GithubRepositories />} />
+            <Route exact path="*" element={<NoPage />} />
           </Routes>
         </div>
         <Footer />
